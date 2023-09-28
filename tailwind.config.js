d6447fe8d3094c753016e2958d46deb6564b48c0
GoodKidJonesy <jo.westh@gmail.com>
2023-09-29 00:02:22 +0200

    Update color-scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.{js,jsx,ts,tsx}',
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        sm: "0.750rem",
+        base: "1rem",
+        xl: "1.333rem",
+        "2xl": "1.777rem",
+        "3xl": "2.369rem",
+        "4xl": "3.158rem",
+        "5xl": "4.210rem",
+      },
+      fontFamily: {
+        heading: "Noto Sans Newa",
+        body: "Noto Sans Newa",
+      },
+      fontWeight: {
+        normal: "400",
+        bold: "700",
+      },
+      colors: {
+        text: "#dde4f8",
+        background: "#060b19",
+        primary: "#5232d2",
+        secondary: "#17061e",
+        accent: "#972bc5",
+      },
+    },
   },
   plugins: [],
-}
-
+};
