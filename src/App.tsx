20440df7a6d542d4b44471fff9383fd378d52bd5
Jonesy <jo.westh@gmail.com>
2023-10-09 09:47:00 +0200

    About Me for Mobile

--- src/App.tsx
+++ src/App.tsx
@@ -18,8 +18,8 @@ function App() {
       <ParticlesComponent />
       <NavBar selected={selected} setSelected={setSelected} />
       {selected === 0 ? <Welcome /> : null}
-      {/*
       {selected === 1 ? <About setSelected={setSelected} /> : null}
+      {/*
       {selected === 2 ? <Portfolio /> : null}
       {selected === 3 ? <Contact /> : null}
       <Socials />
