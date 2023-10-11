f8c1661ab4e6d09ba901e86d672e9f1e9be72e70
GoodKidJonesy <80992821+GoodKidJonesy@users.noreply.github.com>
2023-10-11 15:21:47 +0200

    Fixed bug where duplicate text would appear on buttonpress in navbaritems (#7)

--- src/components/NavBarItem.tsx
+++ src/components/NavBarItem.tsx
@@ -12,9 +12,6 @@ const NavBarItem = (props: {
       onClick={props.onClick}
     >
       <div className="cursor-pointer border-2 p-2 rounded-lg w-full h-full flex items-center justify-center relative border-accent shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#972bc5,0_0_15px_#972bc5,0_0_30px_#972bc5]">
-        <p className="text-xl duration-100 text-accent absolute top-0 left-0 blur-md brightness-200 font-bold">
-          {props.input}
-        </p>
         <p className="text-xl duration-100">{props.input}</p>
       </div>
       <div className="w-full h-full border-2 rounded-lg duration-500 border-accent shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,inset_0_0_5px_#972bc5,inset_0_0_15px_#972bc5,inset_0_0_30px_#972bc5] absolute" />
